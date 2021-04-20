5f26560412ec1a9e3898351b4fda8c63e6392712
mubbsharanwar <mubbsharanwar92@gmail.com>
2021-04-20 14:38:59 +0500

    Add country code in MFEAppContextView
    add new api MFEAppContextView to handle country code
    add unit test MFEAppContextViewTest
    this newly created api will be marged in TPAContextViewTest when margeing code in master branch
    
    VAN-366

--- openedx/core/djangoapps/user_authn/api/urls.py
+++ openedx/core/djangoapps/user_authn/api/urls.py
@@ -4,10 +4,9 @@ Authn API urls
 
 from django.conf.urls import url
 
-from openedx.core.djangoapps.user_authn.api.views import TPAContextView
+from openedx.core.djangoapps.user_authn.api.views import TPAContextView, MFEAppContextView
 
 urlpatterns = [
-    url(
-        r'^third_party_auth_context$', TPAContextView.as_view(), name='third_party_auth_context'
-    ),
+    url(r'^third_party_auth_context$', TPAContextView.as_view(), name='third_party_auth_context'),
+    url(r'^mfe_app_auth_context$', MFEAppContextView.as_view(), name='mfe_app_auth_context'),
 ]
